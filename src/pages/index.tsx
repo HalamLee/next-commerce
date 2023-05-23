d24ffb72d817547c590e28a27219041f7da2500d
halamlee <halam.lee.lami@gmail.com>
2023-05-24 01:56:28 +0900

    design: emotion 사용해보기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import { useEffect, useRef, useState } from 'react';
+import { css } from '@emotion/react';
+import Button from '@/components/Button';
 
 export default function Home() {
   // const [products, setProducts] = useState<
@@ -38,8 +40,25 @@ export default function Home() {
       <Head>
         <title>Lami's Commerce</title>
       </Head>
-      <input ref={inputRef} type="text" placeholder="name" />
-      <button onClick={handleClick}>Add</button>
+      <input
+        ref={inputRef}
+        type="text"
+        placeholder="name"
+        className="placeholder:italic placeholder:text-slate-400 block bg-white w-96 border border-slate-300 rounded-md py-2 pl-3 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+      />
+      <button
+        css={css`
+          background-color: hotpink;
+          padding: 16px;
+          border-radius: 8px;
+        `}
+        onClick={handleClick}
+      >
+        Add
+      </button>
+
+      <Button onClick={handleClick}>Add 2</Button>
+
       <div>
         <p>Product List</p>
         {products &&
