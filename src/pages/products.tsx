cc47b24fea42a220ae446c0d6b2f3f069bb2e134
Lami <Halam.Lee.Lami@gmail.com>
2023-05-25 16:31:34 +0900

    feat: nuka-carousel 사용

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,4 +1,6 @@
-import ImageGallery from 'react-image-gallery';
+import Image from 'next/image';
+import Carousel from 'nuka-carousel';
+import { useState } from 'react';
 
 const images = [
   {
@@ -22,8 +24,8 @@ const images = [
     thumbnail: 'https://picsum.photos/id/1012/250/150/',
   },
   {
-    original: 'https://picsum.photos/id/1016/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1016/250/150/',
+    original: 'https://picsum.photos/id/1019/1000/600/',
+    thumbnail: 'https://picsum.photos/id/1019/250/150/',
   },
   {
     original: 'https://picsum.photos/id/1013/1000/600/',
@@ -32,5 +34,60 @@ const images = [
 ];
 
 export default function Products() {
-  return <ImageGallery items={images} />;
+  const [index, setIndex] = useState(0);
+  return (
+    <>
+      <Carousel
+        animation="zoom"
+        autoplay
+        wrapAround
+        withoutControls
+        slideIndex={index}
+      >
+        {images.map((item) => (
+          <div
+            key={item.original}
+            style={{ position: 'relative', width: '100vw', height: '600px' }}
+          >
+            <Image
+              src={item.original}
+              alt="image"
+              sizes="100vw"
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+        ))}
+      </Carousel>
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100vw',
+          marginTop: '40px',
+        }}
+      >
+        {images.map((item, idx) => (
+          <div
+            key={idx}
+            style={{
+              position: 'relative',
+              width: '100px',
+              height: '50px',
+              cursor: 'pointer',
+            }}
+            onClick={() => setIndex(idx)}
+          >
+            <Image
+              src={item.original}
+              alt="image"
+              sizes="100%"
+              fill
+              style={{ objectFit: 'cover' }}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
